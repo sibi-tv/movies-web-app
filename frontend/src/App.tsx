e917fe3957fc874cb07167dc28359679c943570e
sibi_tiruchi <sibi.tiruchi@gmail.com>
2025-10-04 01:44:09 -0400

    Frontend set up complete with React, TypeScript, and TailwindCSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <div className="flex w-full min-h-screen items-center justify-center text-6xl text-blue-400">
+      <div>Hello World</div>
+    </div>
+  )
+}
+
+export default App
